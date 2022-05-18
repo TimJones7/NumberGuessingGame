dcb6d9fd67eee3849c6990fbdd10823e13b52f88
Tim Jones <timothy.jones@gmx.us>
2022-05-17 20:48:09 -0400

    Add project files.

---
+++ Players/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame.Players
+{
+    //  Parent player class for this game
+    internal class Player
+    {
+
+    }
+}
